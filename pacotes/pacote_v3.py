f5abdb2e4b522b0a1463ce7681d8255024bc0db1
aksilvaa <alison.keuver@gmail.com>
2020-04-16 19:57:56 -0300

    finalizado seção 12 - pacotes

---
+++ pacotes/pacote_v3.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
+
+print('Soma', modulo1.soma(3, 2))
+print('Subtração', modulo1_sub.subtracao(3, 2))
---
+++ pacotes/pacote_v5.py
@@ -0,0 +1,5 @@
+from calc import soma, subtracao
+
+
+print(soma(4, 3))
+print(subtracao(4, 3))
